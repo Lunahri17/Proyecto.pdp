430eb0e0f6a2938fc0532eca51815f41cc2e84e1
Lunahri17 <lunahri@outlook.com>
2021-11-10 18:27:17 -0300

    Metodo findPayments

--- src/sisexpsql/DBpayments.java
+++ src/sisexpsql/DBpayments.java
@@ -47,4 +47,31 @@ public class DBpayments {
             System.out.println(e.getMessage());
         }
     }
+    
+    public DefaultTableModel findPayments(String customerNumber){
+        DefaultTableModel resultado = new DefaultTableModel();
+        resultado.addColumn("Número de Cliente");
+        resultado.addColumn("Código del Comprobante");
+        resultado.addColumn("Fecha de Pago");
+        resultado.addColumn("Cantidad");
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/classicmodels","root","1234");
+            PreparedStatement s = con.prepareStatement(
+                    "SELECT * FROM payments WHERE customerNumber = ?");
+            s.setString(1, customerNumber);
+            ResultSet res = s.executeQuery(); 
+            while (res.next()) {
+                Object[] fila = new Object[4];
+                fila[0] = res.getString("customerNumber");
+                fila[1] = res.getString("checkNumber");
+                fila[2] = res.getString("paymentDate");
+                fila[3] = res.getString("amount");
+                resultado.addRow(fila);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return resultado;
+    }
 }
