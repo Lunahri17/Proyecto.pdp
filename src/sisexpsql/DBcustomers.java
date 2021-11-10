62bd8913cd1482852b48ffafec814d4ed1dfa8c1
Lunahri17 <lunahri@outlook.com>
2021-11-10 11:59:36 -0300

    Método updateCountry

--- src/sisexpsql/DBcustomers.java
+++ src/sisexpsql/DBcustomers.java
@@ -238,6 +238,21 @@ public class DBcustomers {
             System.out.println(e.getMessage());
         }
     }
+
+    public void updateCounrty(String country, String customerNumber){
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/classicmodels","root","1234");
+            PreparedStatement s = con.prepareStatement(
+                    "UPDATE customers SET country = ? WHERE customerNumber = ?");
+            s.setString(1, country);
+            s.setString(2, customerNumber);
+            s.executeUpdate();         
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
     public void prueba(){
         try {
