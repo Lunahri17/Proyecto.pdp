c8d32c8495e61e7505344f0030b7901a7843be69
Lunahri17 <lunahri@outlook.com>
2021-11-10 11:53:25 -0300

    Método updateContactFirstName

--- src/sisexpsql/DBcustomers.java
+++ src/sisexpsql/DBcustomers.java
@@ -133,6 +133,21 @@ public class DBcustomers {
             System.out.println(e.getMessage());
         }
     }
+
+    public void updateContactFirstName(String contactFirstName, String customerNumber){
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/classicmodels","root","1234");
+            PreparedStatement s = con.prepareStatement(
+                    "UPDATE customers SET contactFirstName = ? WHERE customerNumber = ?");
+            s.setString(1, contactFirstName);
+            s.setString(2, customerNumber);
+            s.executeUpdate();         
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
     
     public void prueba(){
