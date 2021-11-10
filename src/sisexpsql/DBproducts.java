d452b89662329e0cee6b5c85446e04cd5cb6b80a
Lunahri17 <lunahri@outlook.com>
2021-11-10 20:28:50 -0300

    Método findProducts en clase DBproducts

--- src/sisexpsql/DBproducts.java
+++ src/sisexpsql/DBproducts.java
@@ -16,7 +16,6 @@ public class DBproducts {
         datos.addColumn("Cantidad en Stock");
         datos.addColumn("Precio de Compra");
         datos.addColumn("MSRP");
-        
         try {
             Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost/classicmodels","root","1234");
@@ -40,6 +39,49 @@ public class DBproducts {
         }
         return datos;
     }
+    
+    public DefaultTableModel findProduct(String productCode){
+        DefaultTableModel datos = new DefaultTableModel();
+        datos.addColumn("Código de Producto");
+        datos.addColumn("Nombre del Producto");
+        datos.addColumn("Linea del Producto");
+        datos.addColumn("Escala del Producto");
+        datos.addColumn("Vendedor del Producto");
+        datos.addColumn("Descripción del Producto");
+        datos.addColumn("Cantidad en Stock");
+        datos.addColumn("Precio de Compra");
+        datos.addColumn("MSRP");
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/classicmodels","root","1234");
+            PreparedStatement s = con.prepareStatement(
+                    "SELECT * FROM products WHERE productCode = ?");
+            s.setString(1, productCode);
+            ResultSet res = s.executeQuery(); 
+            while (res.next()) {
+                Object[] fila = new Object[9];
+                fila[0] = res.getString("productCode");
+                fila[1] = res.getString("productName");
+                fila[2] = res.getString("productLine");
+                fila[3] = res.getString("productScale");
+                fila[4] = res.getString("productVendor");
+                fila[5] = res.getString("productDescription");
+                fila[6] = res.getString("quantityInStock");
+                fila[7] = res.getString("buyPrice");
+                fila[8] = res.getString("MSRP");
+                datos.addRow(fila);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return datos;
+    }
+    
+    
+    
+    
+    
+    
     
     
 }
