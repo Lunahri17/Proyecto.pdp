fa339eee20c2210b70b8991b8a583e3cb60ca2f7
Lunahri17 <lunahri@outlook.com>
2021-11-13 18:32:22 -0300

    Método findOfficeByCode

--- src/sisexpsql/DBoffices.java
+++ src/sisexpsql/DBoffices.java
@@ -57,4 +57,42 @@ public class DBoffices {
         return datos;
     }
     
+    public DefaultTableModel findOfficeByCode(String officeCode){
+        DefaultTableModel datos = new DefaultTableModel();
+        datos.addColumn("Código de Oficina");
+        datos.addColumn("Ciudad");
+        datos.addColumn("Teléfono");
+        datos.addColumn("Dirección");
+        datos.addColumn("Dirección Alternativa");
+        datos.addColumn("Provincia");
+        datos.addColumn("País");
+        datos.addColumn("Código Postal");
+        datos.addColumn("Territorio");
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost/classicmodels","root","1234");
+            PreparedStatement s = con.prepareStatement(
+                    "SELECT * FROM offices WHERE officeCode LIKE ?");
+            s.setString(1,"%" + officeCode + "%");
+            ResultSet res = s.executeQuery(); 
+            while (res.next()) {
+                Object[] fila = new Object[9];
+                fila[0] = res.getString("officeCode");
+                fila[1] = res.getString("city");
+                fila[2] = res.getString("phone");
+                fila[3] = res.getString("addressLine1");
+                fila[4] = res.getString("addressLine2");
+                fila[5] = res.getString("state");
+                fila[6] = res.getString("country");
+                fila[7] = res.getString("postalCode");
+                fila[8] = res.getString("territory");
+                datos.addRow(fila);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return datos;
+    }
+    
+    
 }
