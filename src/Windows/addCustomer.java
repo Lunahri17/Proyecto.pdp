45b9ff3a2ea15eebc79d4c3bf1bf2942031940db
Lunahri17 <lunahri@outlook.com>
2021-11-18 19:57:43 -0300

    validación con ventanas para ucando estan vacios

--- src/Windows/addCustomer.java
+++ src/Windows/addCustomer.java
@@ -333,7 +333,6 @@ public class addCustomer extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-   
         if (validateTextFields()){
             DBcustomers db = new DBcustomers();
             db.addCustomer(customerNumberTextField.getText(), customerNameTextField.getText(), 
